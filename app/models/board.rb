f189829625b28e0adf59e63bb9882d1c233ba059
宮澤昭海 <>
2022-05-16 17:34:45 +0900

    NewBoardページの作成

---
+++ app/models/board.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id          :integer          not null, primary key
+#  description :text             not null
+#  name        :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer          not null
+#
+# Indexes
+#
+#  index_boards_on_user_id  (user_id)
+#
+class Board < ApplicationRecord
+end
